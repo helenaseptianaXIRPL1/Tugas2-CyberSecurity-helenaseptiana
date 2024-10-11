bf8611253789f712d10036fec78cfe8dc9d00db0
HelenaS <88118215+helenaseptianaXIRPL1@users.noreply.github.com>
2024-10-11 14:11:16 +0700

    Add files via upload

---
+++ Filetxt/Filetxt/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        BrowseDirectoryToolStripMenuItem = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        OpenFolderToolStripMenuItem = New ToolStripMenuItem()
+        SaveToolStripMenuItem1 = New ToolStripMenuItem()
+        SaveAsToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        EditToolStripMenuItem = New ToolStripMenuItem()
+        CopyToolStripMenuItem = New ToolStripMenuItem()
+        PasteToolStripMenuItem = New ToolStripMenuItem()
+        CutToolStripMenuItem = New ToolStripMenuItem()
+        ListBox1 = New ListBox()
+        BrowseDirectoryToolStripMenuItem.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Dock = DockStyle.Right
+        TextBox1.Location = New Point(234, 24)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(355, 331)
+        TextBox1.TabIndex = 0
+        ' 
+        ' BrowseDirectoryToolStripMenuItem
+        ' 
+        BrowseDirectoryToolStripMenuItem.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, EditToolStripMenuItem})
+        BrowseDirectoryToolStripMenuItem.Location = New Point(0, 0)
+        BrowseDirectoryToolStripMenuItem.Name = "BrowseDirectoryToolStripMenuItem"
+        BrowseDirectoryToolStripMenuItem.Size = New Size(589, 24)
+        BrowseDirectoryToolStripMenuItem.TabIndex = 1
+        BrowseDirectoryToolStripMenuItem.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {NewToolStripMenuItem, OpenToolStripMenuItem, OpenFolderToolStripMenuItem, SaveToolStripMenuItem1, SaveAsToolStripMenuItem, ExitToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.Size = New Size(180, 22)
+        NewToolStripMenuItem.Text = "New"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.Size = New Size(180, 22)
+        OpenToolStripMenuItem.Text = "Open File"
+        ' 
+        ' OpenFolderToolStripMenuItem
+        ' 
+        OpenFolderToolStripMenuItem.Name = "OpenFolderToolStripMenuItem"
+        OpenFolderToolStripMenuItem.Size = New Size(180, 22)
+        OpenFolderToolStripMenuItem.Text = "Open Folder"
+        ' 
+        ' SaveToolStripMenuItem1
+        ' 
+        SaveToolStripMenuItem1.Name = "SaveToolStripMenuItem1"
+        SaveToolStripMenuItem1.Size = New Size(180, 22)
+        SaveToolStripMenuItem1.Text = "Save"
+        ' 
+        ' SaveAsToolStripMenuItem
+        ' 
+        SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        SaveAsToolStripMenuItem.Size = New Size(180, 22)
+        SaveAsToolStripMenuItem.Text = "Save As"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(180, 22)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' EditToolStripMenuItem
+        ' 
+        EditToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {CopyToolStripMenuItem, PasteToolStripMenuItem, CutToolStripMenuItem})
+        EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        EditToolStripMenuItem.Size = New Size(39, 20)
+        EditToolStripMenuItem.Text = "Edit"
+        ' 
+        ' CopyToolStripMenuItem
+        ' 
+        CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        CopyToolStripMenuItem.Size = New Size(102, 22)
+        CopyToolStripMenuItem.Text = "Copy"
+        ' 
+        ' PasteToolStripMenuItem
+        ' 
+        PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        PasteToolStripMenuItem.Size = New Size(102, 22)
+        PasteToolStripMenuItem.Text = "Paste"
+        ' 
+        ' CutToolStripMenuItem
+        ' 
+        CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        CutToolStripMenuItem.Size = New Size(102, 22)
+        CutToolStripMenuItem.Text = "Cut"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(0, 25)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(228, 334)
+        ListBox1.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(589, 355)
+        Controls.Add(ListBox1)
+        Controls.Add(TextBox1)
+        Controls.Add(BrowseDirectoryToolStripMenuItem)
+        MainMenuStrip = BrowseDirectoryToolStripMenuItem
+        Name = "Form1"
+        Text = "Form1"
+        BrowseDirectoryToolStripMenuItem.ResumeLayout(False)
+        BrowseDirectoryToolStripMenuItem.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BrowseDirectoryToolStripMenuItem As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents OpenFolderToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem1 As ToolStripMenuItem
+
+End Class
---
+++ Filetxt/Filetxt/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports System.IO
+
+Public Class Form1
+    ' Deklarasi folderBrowser sebagai variabel kelas
+    Private folderBrowser As New FolderBrowserDialog()
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        TextBox1.Clear()
+    End Sub
+
+    Private currentFilePath As String = String.Empty
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Dim openFile As New OpenFileDialog()
+        openFile.Filter = "Text Files|*.txt"
+        If openFile.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = File.ReadAllText(openFile.FileName)
+        End If
+
+        currentFilePath = openFile.FileName
+    End Sub
+
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+        Dim saveFile As New SaveFileDialog()
+        saveFile.Filter = "Text Files|*.txt"
+        If saveFile.ShowDialog() = DialogResult.OK Then
+            File.WriteAllText(saveFile.FileName, TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        TextBox1.Cut()
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        TextBox1.Copy()
+    End Sub
+
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        TextBox1.Paste()
+    End Sub
+
+    Private Sub OpenFolderToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenFolderToolStripMenuItem.Click
+        If folderBrowser.ShowDialog() = DialogResult.OK Then
+            Dim selectedFolder As String = folderBrowser.SelectedPath
+            DisplayFilesInListBox(selectedFolder)
+        End If
+    End Sub
+
+    Private Sub DisplayFilesInListBox(folderPath As String)
+        ListBox1.Items.Clear()
+
+        Try
+            Dim files As String() = Directory.GetFiles(folderPath)
+
+            For Each file As String In files
+                ListBox1.Items.Add(Path.GetFileName(file))
+            Next
+        Catch ex As Exception
+            MessageBox.Show("Terjadi kesalahan saat membaca folder: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedItem IsNot Nothing Then
+            ' Mendapatkan nama file yang dipilih
+            Dim selectedFile As String = ListBox1.SelectedItem.ToString()
+
+            ' Mendapatkan path folder yang saat ini dipilih
+            Dim folderPath As String = folderBrowser.SelectedPath
+
+            ' Menggabungkan folder path dan nama file untuk mendapatkan path lengkap
+            Dim fullPath As String = Path.Combine(folderPath, selectedFile)
+
+            ' Membaca isi file dan menampilkannya di TextBox1
+            If File.Exists(fullPath) Then
+                TextBox1.Text = File.ReadAllText(fullPath)
+            Else
+                MessageBox.Show("File tidak ditemukan: " & fullPath, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        End If
+    End Sub
+
+    Private Sub SaveToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem1.Click
+        If String.IsNullOrEmpty(currentFilePath) Then
+            ' Jika tidak ada nama file yang sudah disimpan, gunakan SaveFileDialog
+            Dim saveFile As New SaveFileDialog()
+            saveFile.Filter = "Text Files|*.txt"
+
+            If saveFile.ShowDialog() = DialogResult.OK Then
+                Try
+                    ' Menyimpan isi TextBox1 ke dalam file yang dipilih
+                    File.WriteAllText(saveFile.FileName, TextBox1.Text)
+                    currentFilePath = saveFile.FileName ' Simpan path file
+                    MessageBox.Show("File berhasil disimpan!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Catch ex As Exception
+                    MessageBox.Show("Terjadi kesalahan saat menyimpan file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+            End If
+        Else
+            ' Jika sudah ada nama file, simpan langsung tanpa dialog
+            Try
+                File.WriteAllText(currentFilePath, TextBox1.Text)
+                MessageBox.Show("File berhasil disimpan!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Terjadi kesalahan saat menyimpan file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+End Class
+
+
